a7ca44c57bab085fb8255fb9f25687d13a15ed55
nalle94 <nrthornval@hotmail.com>
2020-11-07 20:18:16 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,21 +1,25 @@
 ﻿#!/usr/bin/env python3
 import sys
 import re
+import gzip
 
 #### 2. Detect Phred score ####
 infile=open("sequence_example.txt","r")
 
 
 
-quality_table = {
-'!': 0, '"': 1, '#': 2, '$': 3, '%': 4, '&': 5, "'": 6, '(': 7, ')': 8, '*': 9, 
-'+': 10, ',': 11, '-': 12, '.': 13, '/': 14, '0': 15, '1': 16, '2': 17, '3': 18, '4': 19,
-'5': 20, '6': 21, '7': 22, '8': 23, '9': 24, ':': 25, ';': 26, '<': 27, '=': 28, '>': 29,
-'?': 30, '@': 31, 'A': 32, 'B': 33, 'C': 34, 'D': 35, 'E': 36, 'F': 37, 'G': 38, 'H': 39,
-'I': 40, 'J': 41, 'K': 42, 'L': 43, 'M': 44, 'N': 45, 'O': 46, 'P': 47, 'Q': 48, 'R': 49,
-'S': 50, 'T': 51, 'U': 52, 'V': 53, 'W': 54, 'X': 55, 'Y': 56, 'Z': 57, '[': 58, '\\': 59,
-']': 60, '^': 61, '_': 62, '`': 63, 'a': 64, 'b': 65, 'c': 66, 'd': 67, 'e': 68, 'f': 69,
-'g': 70, 'h': 71}
+phred33 = {
+	'!': 0, '"': 1, '#': 2, '$': 3, '%': 4, '&': 5, "'": 6, '(': 7, ')': 8, '*': 9, 
+	'+': 10, ',': 11, '-': 12, '.': 13, '/': 14, '0': 15, '1': 16, '2': 17, '3': 18, '4': 19,
+	'5': 20, '6': 21, '7': 22, '8': 23, '9': 24, ':': 25, ';': 26, '<': 27, '=': 28, '>': 29,
+	'?': 30, '@': 31, 'A': 32, 'B': 33, 'C': 34, 'D': 35, 'E': 36, 'F': 37, 'G': 38, 'H': 39,
+	'I': 40, 'J': 41}
+phred64 = {
+	'@': 0, 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8,	'I': 9,
+	'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19,
+	'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 35, 'Z': 26, '[': 27, '\\': 28, ']': 29,
+	'^': 30, '_': 31, '`': 32, 'a': 33, 'b': 34, 'c': 35, 'd': 36, 'e': 37, 'f': 38, 'g': 39, 
+	'h': 40, 'i': 41}
 
 
 
@@ -34,10 +38,11 @@ def detect_phred(infile):
             if line_count % 4 == 0:
                 for char in line:
                     #translate the line to find phred scales
-                    if quality_table[char] < 31:
+                    if char in phred33 and char not in phred64:
                         phred_scale = "phred+33"
-                    elif quality_table[char] >= 31:
+                    elif char in phred64 and char not in phred33:
                         phred_scale = "phred+64"
+    #if program can not determine phred scale
     if phred_scale == None:
         print("Cannot determine phred scale")
     return phred_scale    
